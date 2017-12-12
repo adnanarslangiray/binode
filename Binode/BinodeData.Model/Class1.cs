433c2d90fe138d34a6d70f054a3d683e864fd528
BEM <BEM@DESKTOP-S3O5AOR>
2017-12-12 13:35:33 +0300

    ddf

---
+++ Binode/BinodeData.Model/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binode.Data.Model
+{
+    public class Kategori
+    {
+        public int Id { get; set; }
+        public string Isim { get; set; }
+        public int AciklamaNotu { get; set; }
+        public Kategori UstKategori { get; set; }
+        public int SiraNumarasi { get; set; }
+        public List<Kategori> AltKategori { get; set; }
+        public List<Icerik> Icerik { get; set; }
+
+    }
+}
